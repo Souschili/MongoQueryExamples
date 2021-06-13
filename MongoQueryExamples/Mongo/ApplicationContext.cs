1c82ff386c4f12a7afee5e9d59adc5fc7abe5303
Orxan_8km <aliyevorhan82@gmail.com>
2021-06-13 22:58:22 +0400

    Добавьте файлы проекта.

---
+++ MongoQueryExamples/Mongo/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace MongoQueryExamples.Mongo
+{
+    class ApplicationContext
+    {
+        IMongoCollection<object> PersonCollection { get; set; }
+
+        public ApplicationContext()
+        {
+            IMongoClient client = new MongoClient();
+            IMongoDatabase database = client.GetDatabase("TestQueryDB");
+            this.PersonCollection = database.GetCollection<object>("Persons");
+        }
+    }
+}
